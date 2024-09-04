6297545aa933de0591060a9f62951785de00c68a
t3rb3d <terbed@gmail.com>
2024-09-04 18:30:44 +0200

    customisation

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,25 +1,26 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'All rights reserved',
+  author: 'Dániel Terbe',
+  headerTitle: "t3rb3d's blog",
+  description:
+    'Welcome 👋 This is my dev blog where I write about my experiences and thoughts. I also update my projects and resume here.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://terbed.dev',
+  siteRepo: 'https://github.com/terbed/devblog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  x: 'https://twitter.com/x',
+  email: 'terbed@gmail.com',
+  github: 'https://github.com/terbed',
+  x: 'https://x.com/t3rb3d4',
   // twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  threads: 'https://www.threads.net',
-  instagram: 'https://www.instagram.com',
+  facebook: '',
+  youtube: '',
+  linkedin: 'https://www.linkedin.com/in/terbed/',
+  threads: '',
+  instagram: '',
   locale: 'en-US',
   // set to true if you want a navbar fixed to the top
   stickyNav: false,
@@ -50,7 +51,7 @@ const siteMetadata = {
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
+    provider: '',
   },
   comments: {
     // If you want to use an analytics provider you have to add it to the
