03dc29e32db59226748d346c04a13ce5221cc6d3
t3rb3d <terbed@gmail.com>
2024-09-11 23:20:44 +0200

    adds contact info and publications finished

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -43,35 +43,91 @@ const ResumePage = () => {
 
   return (
     <div className="container mx-auto py-10">
-      {/*<h1 className="mb-6 text-4xl font-bold dark:text-white">My Resume</h1>*/}
-
       <div ref={resumeRef} className="rounded-md bg-white p-8 shadow-lg dark:bg-gray-800">
+        {/* Header Section */}
         <div className="divide-y divide-gray-200 dark:divide-gray-700">
           {/* Name, Title, and Contact Section */}
-          <div className="flex items-start justify-between pb-8 pt-6 md:space-y-5">
+          <div className="flex flex-col items-center space-y-4 pb-8 pt-6 md:flex-row md:justify-between md:space-y-0">
             {/* Left: Name and Title */}
-            <div>
-              <h1 className="font-extrabold tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+            <div className="text-center md:text-left">
+              <h1 className="text-5xl font-extrabold tracking-tight text-gray-900 dark:text-gray-100">
                 Dániel Terbe
               </h1>
-              <p className="text-2xl text-gray-700 dark:text-gray-300">AI Researcher & Developer</p>
+              <p className="mt-2 text-2xl text-gray-700 dark:text-gray-300">
+                AI Researcher & Developer
+              </p>
             </div>
 
             {/* Right: Contact Information */}
-            <div className="text-right">
-              <p className="text-xl text-gray-500 dark:text-gray-400">
-                Contact: <br />
+            <div className="flex flex-col space-y-2 text-right">
+              {/* Email */}
+              <div className="flex items-center space-x-2">
+                <svg
+                  fill="currentColor"
+                  width="24px"
+                  height="24px"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="text-black dark:text-white"
+                  viewBox="0 0 128 128"
+                >
+                  <path d="M123.25 24.192c0-.018 0-.034-.005-.052s-.007-.063-.009-.094a1.734 1.734 0 0 0-.083-.408c-.006-.018 0-.037-.011-.055s-.01-.015-.013-.023a1.734 1.734 0 0 0-.227-.407c-.021-.028-.043-.053-.066-.08a1.755 1.755 0 0 0-.31-.294c-.012-.009-.022-.02-.034-.028a1.744 1.744 0 0 0-.414-.2c-.034-.012-.068-.022-.1-.032a1.733 1.733 0 0 0-.474-.073H6.5a1.733 1.733 0 0 0-.474.073c-.035.01-.068.02-.1.032a1.744 1.744 0 0 0-.414.2c-.012.008-.022.019-.034.028a1.755 1.755 0 0 0-.31.294c-.022.027-.045.052-.066.08a1.734 1.734 0 0 0-.227.407c0 .008-.01.015-.013.023s-.005.037-.011.055a1.734 1.734 0 0 0-.083.408c0 .032-.009.063-.009.094s-.005.034-.005.052v79.615c0 .023.006.045.007.068a1.737 1.737 0 0 0 .019.188c.008.051.015.1.027.152a1.74 1.74 0 0 0 .056.179c.017.047.033.094.054.139a1.729 1.729 0 0 0 .093.172c.024.04.048.081.075.119a1.743 1.743 0 0 0 .125.152c.033.036.066.072.1.106.021.019.037.042.059.061s.036.017.052.03a1.736 1.736 0 0 0 .452.263c.035.014.071.022.107.033a1.732 1.732 0 0 0 .488.085c.012 0 .023.006.035.006H121.501c.012 0 .023-.006.034-.006a1.732 1.732 0 0 0 .489-.085c.035-.011.07-.019.1-.033a1.736 1.736 0 0 0 .453-.263c.016-.013.036-.017.052-.03s.038-.042.059-.061c.036-.034.069-.069.1-.106a1.743 1.743 0 0 0 .125-.152c.027-.038.051-.078.075-.119a1.729 1.729 0 0 0 .093-.172c.021-.045.037-.092.054-.139a1.74 1.74 0 0 0 .056-.179c.012-.05.019-.1.027-.152a1.737 1.737 0 0 0 .019-.188c0-.023.007-.045.007-.068zM45.8 60.316l17.058 14.677a1.751 1.751 0 0 0 2.283 0L82.2 60.316l35.512 41.741H10.289zM8.25 99.052V28.007l34.9 30.026zm76.6-41.019 34.9-30.026v71.045zm31.931-32.091L81.276 56.493c-.006.005-.014.008-.02.014l-.019.02L64 71.358 46.763 56.527l-.019-.02-.02-.014-35.507-30.551z" />
+                </svg>
                 <a
                   href="mailto:terbed@gmail.com"
-                  className="text-primary-500 hover:underline dark:text-primary-400"
+                  className="textbf text-primary-500 hover:underline dark:text-primary-400"
                 >
                   terbed@gmail.com
                 </a>
-              </p>
+              </div>
+
+              {/* LinkedIn */}
+              <div className="flex items-center space-x-2">
+                <svg
+                  role="img"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-black dark:text-white"
+                  fill="currentColor"
+                >
+                  <title>LinkedIn</title>
+                  <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+                </svg>
+                <a
+                  href="https://www.linkedin.com/in/terbed"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="textbf text-primary-500 hover:underline dark:text-primary-400"
+                >
+                  linkedin.com/in/terbed
+                </a>
+              </div>
+
+              {/* GitHub */}
+              <div className="flex items-center space-x-2">
+                <svg
+                  role="img"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-black dark:text-white"
+                  fill="currentColor"
+                >
+                  <title>GitHub</title>
+                  <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+                </svg>
+                <a
+                  href="https://github.com/terbed"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="textbf text-primary-500 hover:underline dark:text-primary-400"
+                >
+                  github.com/terbed
+                </a>
+              </div>
             </div>
           </div>
 
-          <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0"></div>
+          {/* Single Separator Line */}
+          <hr className="my-4 border-gray-300 dark:border-gray-700" />
         </div>
 
         {/* ------------------------ Experience Section ----------------------------------------- */}
@@ -222,6 +278,7 @@ const ResumePage = () => {
             {/* Add more education items in the same format */}
           </div>
         </div>
+
         {/* Publications */}
         <div className="mt-12">
           {/* Adjust the title to be centered on the timeline */}
@@ -229,54 +286,70 @@ const ResumePage = () => {
             Publications
           </h3>
           <div className="relative mt-8">
-            {/* Adjust line and dot position */}
+            {/* Timeline line */}
             <div className="absolute left-1/4 h-full w-1 bg-primary-500"></div>
 
             {/* Publication Items */}
             <div className="mb-8 flex items-start">
               <div className="relative w-1/4 pr-4 text-right">
-                <span className="block text-gray-500 dark:text-gray-400">2024</span>
+                <span className="block text-gray-500 dark:text-gray-400">January 2024</span>
                 <div className="absolute right-[-10px] top-2 h-4 w-4 rounded-full bg-primary-500"></div>
               </div>
               <div className="w-3/4 pl-12">
-                <div className="milestone-item">
-                  <h4 className="text-lg font-bold">
-                    Terbe, D.; Orzó, L.; Bicsák, B.; Zarándy, Á.
-                  </h4>
-                  <p>
-                    <strong>Hologram Noise Model for Data Augmentation and Deep Learning.</strong>{' '}
-                    Sensors 2024, 24, 948.{' '}
-                    <a
-                      href="https://doi.org/10.3390/s24030948"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-primary-500 hover:underline"
-                    >
-                      Read Here
-                    </a>
-                  </p>
+                <div className="milestone-item relative overflow-hidden">
+                  {/* Background Noise Image */}
+                  <img
+                    src="/static/cv/noisy-texture-black.png" // Path to your noise image
+                    alt="Noise Background"
+                    className="pointer-events-none absolute inset-0 z-0 h-full w-full object-cover opacity-10"
+                  />
+
+                  {/* Foreground Content */}
+                  <div className="relative z-10">
+                    <h4 className="mb-2 text-lg font-bold text-gray-900 dark:text-gray-100">
+                      Hologram Noise Model for Data Augmentation and Deep Learning
+                    </h4>
+                    <p className="text-gray-600 dark:text-gray-300">
+                      Terbe, D.; Orzó, L.; Bicsák, B.; Zarándy, Á.
+                    </p>
+                    <p className="mb-1 text-primary-500 dark:text-primary-400">
+                      Sensors 2024, 24, 948. <strong>DOI:</strong>{' '}
+                      <a
+                        href="https://doi.org/10.3390/s24030948"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-primary-500 hover:underline dark:text-primary-400"
+                      >
+                        https://doi.org/10.3390/s24030948
+                      </a>
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
 
             <div className="mb-8 flex items-start">
               <div className="relative w-1/4 pr-4 text-right">
-                <span className="block text-gray-500 dark:text-gray-400">2022</span>
+                <span className="block text-gray-500 dark:text-gray-400">October 2022</span>
                 <div className="absolute right-[-10px] top-2 h-4 w-4 rounded-full bg-primary-500"></div>
               </div>
               <div className="w-3/4 pl-12">
                 <div className="milestone-item">
-                  <h4 className="text-lg font-bold">Terbe, D.; Orzó, L.; Zarándy, Á.</h4>
-                  <p>
-                    <strong>Classification of Holograms with 3D-CNN.</strong> Sensors 2022, 22,
-                    8366.{' '}
+                  <h4 className="mb-2 text-lg font-bold text-gray-900 dark:text-gray-100">
+                    Classification of Holograms with 3D-CNN
+                  </h4>
+                  <p className="text-gray-600 dark:text-gray-300">
+                    Terbe, D.; Orzó, L.; Zarándy, Á.
+                  </p>
+                  <p className="mb-1 text-primary-500 dark:text-primary-400">
+                    Sensors 2022, 22, 8366. <strong>DOI:</strong>{' '}
                     <a
                       href="https://doi.org/10.3390/s22218366"
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="text-primary-500 hover:underline"
+                      className="text-primary-500 hover:underline dark:text-primary-400"
                     >
-                      Read Here
+                      https://doi.org/10.3390/s22218366
                     </a>
                   </p>
                 </div>
@@ -285,30 +358,62 @@ const ResumePage = () => {
 
             <div className="mb-8 flex items-start">
               <div className="relative w-1/4 pr-4 text-right">
-                <span className="block text-gray-500 dark:text-gray-400">2021</span>
+                <span className="block text-gray-500 dark:text-gray-400">October 2021</span>
                 <div className="absolute right-[-10px] top-2 h-4 w-4 rounded-full bg-primary-500"></div>
               </div>
               <div className="w-3/4 pl-12">
                 <div className="milestone-item">
-                  <h4 className="text-lg font-bold">Terbe, D.; László, O.; Zarándy, Á.</h4>
-                  <p>
-                    <strong>
-                      Deep-learning-based bright-field image generation from a single hologram using
-                      an unpaired dataset.
-                    </strong>{' '}
-                    Optics Letters 46.22 (2021): 5567-5570.{' '}
+                  <h4 className="mb-2 text-lg font-bold text-gray-900 dark:text-gray-100">
+                    Deep-learning-based bright-field image generation from a single hologram using
+                    an unpaired dataset
+                  </h4>
+                  <p className="text-gray-600 dark:text-gray-300">
+                    Terbe, D.; László, O.; Zarándy, Á.
+                  </p>
+                  <p className="mb-1 text-primary-500 dark:text-primary-400">
+                    Optics Letters 46.22 (2021): 5567-5570. <strong>DOI:</strong>{' '}
                     <a
                       href="https://doi.org/10.1364/OL.440900"
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="text-primary-500 hover:underline"
+                      className="text-primary-500 hover:underline dark:text-primary-400"
                     >
-                      Read Here
+                      https://doi.org/10.1364/OL.440900
                     </a>
                   </p>
                 </div>
               </div>
             </div>
+
+            <div className="mb-8 flex items-start">
+              <div className="relative w-1/4 pr-4 text-right">
+                <span className="block text-gray-500 dark:text-gray-400">July 2021</span>
+                <div className="absolute right-[-10px] top-2 h-4 w-4 rounded-full bg-primary-500"></div>
+              </div>
+              <div className="w-3/4 pl-12">
+                <div className="milestone-item">
+                  <h4 className="mb-2 text-lg font-bold text-gray-900 dark:text-gray-100">
+                    Continuous Camera-Based Premature-Infant Monitoring Algorithms for NICU
+                  </h4>
+                  <p className="text-gray-600 dark:text-gray-300">
+                    Nagy, Á.; Földesy, P.; Jánoki, I.; Terbe, D.; Siket, M.; Szabó, M.; Varga, J.;
+                    Zarándy, Á.
+                  </p>
+                  <p className="mb-1 text-primary-500 dark:text-primary-400">
+                    Applied Sciences. 2021; 11(16):7215. <strong>DOI:</strong>{' '}
+                    <a
+                      href="https://doi.org/10.3390/app11167215"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-primary-500 hover:underline dark:text-primary-400"
+                    >
+                      https://doi.org/10.3390/app11167215
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+
             {/* Add more publication items similarly */}
           </div>
         </div>
