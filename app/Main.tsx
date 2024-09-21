ded1bc56d069f49679784f33b7dd2017f0ad0ffc
t3rb3d <terbed@gmail.com>
2024-09-21 16:07:19 +0200

    newsletter netlify function router

--- app/Main.tsx
+++ app/Main.tsx
@@ -83,7 +83,7 @@ export default function Home({ posts }) {
       )}
       {siteMetadata.newsletter?.provider && (
         <div className="flex items-center justify-center pt-4">
-          <NewsletterForm apiUrl="/netlify/functions/newsletter" />
+          <NewsletterForm apiUrl="/.netlify/functions/newsletter" />
         </div>
       )}
     </>
