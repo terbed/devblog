c3e0e74b4ba46387a2f680ef8831dd2a8cbd87d3
t3rb3d <terbed@gmail.com>
2024-12-09 14:44:12 +0100

    update email

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -87,7 +87,7 @@ const ResumePage = () => {
                   <path d="M1920 428.266v1189.54l-464.16-580.146-88.203 70.585 468.679 585.904H83.684l468.679-585.904-88.202-70.585L0 1617.805V428.265l959.944 832.441L1920 428.266ZM1919.932 226v52.627l-959.943 832.44L.045 278.628V226h1919.887Z" />
                 </svg>
                 <a
-                  href="mailto:terbed@gmail.com"
+                  href="mailto:daniel@terbe.dev"
                   className="textbf text-primary-500 hover:underline dark:text-primary-400"
                 >
                   daniel@terbe.dev
